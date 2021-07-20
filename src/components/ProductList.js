a655bcaf6784c4342a29f0db14517845959d3c1d
Sraddha <8770473516sraddha@gmail.com>
2021-07-20 17:21:18 +0530

    minor fix

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -9,7 +9,7 @@ export default class ProductList extends Component {
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
-                    <Title name="Our" title="Products"/>
+                    <Title name="My" title="Products"/>
                        <div className="row">
                          <ProductConsumer>
                              {value=>{
